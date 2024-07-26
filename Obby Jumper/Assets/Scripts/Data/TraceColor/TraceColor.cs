485071f386269e22ddeed64e70af8c5c060edb6c
FishyFish3r <danyakit394@gmail.com>
2024-07-26 04:44:05 +0300

    Shades

--- Obby Jumper/Assets/Scripts/Data/TraceColor/TraceColor.cs
+++ Obby Jumper/Assets/Scripts/Data/TraceColor/TraceColor.cs
@@ -7,13 +7,13 @@ public class TraceColor : ScriptableObject
 {
     public int ShadesCount
     {
-        get => _colors.Length;
+        get => _shades.Length;
     }
 
-    [SerializeField] private Color[] _colors = new Color[5];
+    [SerializeField] private Color[] _shades = new Color[5];
 
     public Color GetShade(int id)
     {
-        return _colors[id];
+        return _shades[id];
     }
 }
