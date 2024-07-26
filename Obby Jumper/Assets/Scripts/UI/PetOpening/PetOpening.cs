06decac15549ad8d7b9e68d23f67c6475395f8d6
FishyFish3r <danyakit394@gmail.com>
2024-07-26 07:02:42 +0300

    Inventory Egg Opening

--- Obby Jumper/Assets/Scripts/UI/PetOpening/PetOpening.cs
+++ Obby Jumper/Assets/Scripts/UI/PetOpening/PetOpening.cs
@@ -190,7 +190,8 @@ public class PetOpening : MonoBehaviour
 
         PetItem item = (PetItem)_petInventory.GetItemById(id);
         _rewardPreview.Show(item.Icon, _translator.CurrentLangunage == LanguageTranslator.Languages.Russian ? item.RuLabel : item.EnLabel);
-        _hider.ShowOther(gameObject);
+        _hider.ReturnToDefault();
+        //_hider.ShowOther(gameObject);
         gameObject.SetActive(false);
     }
 
