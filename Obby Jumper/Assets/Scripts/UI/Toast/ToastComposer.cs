bf337093a1d5a77faf54e5397bc215432ff930ef
Gangstar Vegas <chochokizachidabz@gmail.com>
2024-08-01 05:54:32 +0300

    Toast-Composer 0.5

--- Obby Jumper/Assets/Scripts/UI/Toast/ToastComposer.cs
+++ Obby Jumper/Assets/Scripts/UI/Toast/ToastComposer.cs
@@ -1,40 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.WSA;
 
-public class Toast : MonoBehaviour
+public class ToastComposer : MonoBehaviour
 {
     public Vector3 TargetPositionOffset
     {
         set => _targetPositionOffset = value;
     }
 
+    [SerializeField] private GameObject _toastPrefab;
     [SerializeField] private RectTransform _rTransform;
     [SerializeField] private float _pathDuration;
     [SerializeField] private float _pauseDuration;
     [SerializeField] private Vector2 _targetPositionOffset;
 
+    private int _currentToastsCount;
     private Vector3 _startPosition;
     private Vector3 _targetPosition;
 
     private void Start()
     {
         _startPosition = _rTransform.anchoredPosition;
-        _targetPosition = _rTransform.anchoredPosition - (Vector2.up * _rTransform.rect.size.y) - _targetPositionOffset;
-
         StartCoroutine(AnimationRoutine());
     }
 
     private IEnumerator AnimationRoutine()
     {
+        GameObject toast = Instantiate(_toastPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        //TextMeshProUGUI text_toast = toast.GetComponent<TMP_Text>;
+        // toast.transform.position.y = toast.rect.size.y/2;
+        
+        _currentToastsCount += 1;
+        _targetPosition = (_rTransform.anchoredPosition - (Vector2.up * _rTransform.rect.size.y) - _targetPositionOffset)*_currentToastsCount;
+
         float timeElapsed = 0;
         while (timeElapsed < _pathDuration) 
         {
             float t = timeElapsed / _pathDuration; 
-            t = t * t * (3f - 2f * t); // https://youtu.be/-VwNSKeQNm4?si=BWS5oio3QO2EX8G1
+            t = t * t * (3f - 2f * t);
             _rTransform.anchoredPosition = Vector2.Lerp(_startPosition, _targetPosition, t);
             timeElapsed += Time.deltaTime;
-            yield return null; //wait for next frame
+            yield return null;
         }
 
         yield return new WaitForSeconds(_pauseDuration);
@@ -43,12 +53,12 @@ public class Toast : MonoBehaviour
         while (timeElapsed < _pathDuration)
         {
             float t = timeElapsed / _pathDuration;
-            t = t * t * (3f - 2f * t); // https://youtu.be/-VwNSKeQNm4?si=BWS5oio3QO2EX8G1
+            t = t * t * (3f - 2f * t);
             _rTransform.anchoredPosition = Vector2.Lerp(_targetPosition, _startPosition, t);
             timeElapsed += Time.deltaTime;
-            yield return null; //wait for next frame
+            yield return null;
         }
-
+        _currentToastsCount -= 1;
         Destroy(gameObject);
     }
 }
