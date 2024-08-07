a563d00478386fe711f8e36cbd2de35d2b062db0
Gangstar Vegas <chochokizachidabz@gmail.com>
2024-08-07 08:58:38 +0300

    UPD

--- Obby Jumper/Assets/Scripts/World/JumpRocksHolder.cs
+++ Obby Jumper/Assets/Scripts/World/JumpRocksHolder.cs
@@ -7,14 +7,19 @@ public class JumpRocksHolder : MonoBehaviour
 {
     [SerializeField] private GameObject[] _jumpRocksPrefabs;
     [SerializeField] private Transform _jumpRocksHolder;
-    [SerializeField] private GameObject _jumpRocks;
+    [SerializeField] private GameObject _checkPoint;
+    private GameObject _jumpRocks;
     private int _currentPrefabId = 0;
+
     public void SpawnRocks()
     {
-        Destroy(_jumpRocks);
+        if (_jumpRocks != null) 
+        {
+            Destroy(_jumpRocks);
+        }
         _jumpRocks = Instantiate(_jumpRocksPrefabs[_currentPrefabId], _jumpRocksHolder);
         _currentPrefabId = _currentPrefabId == _jumpRocksPrefabs.Length - 1 ?  0 : _currentPrefabId + 1;
-        
+        _checkPoint.SetActive(true);
     }
 
     public void Start()
