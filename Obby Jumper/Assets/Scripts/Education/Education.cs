7fd75d4029d8a865dec01a561a0c5325105dd158
Gangstar Vegas <chochokizachidabz@gmail.com>
2024-08-09 07:25:49 +0300

    Add Points

--- Obby Jumper/Assets/Scripts/Education/Education.cs
+++ Obby Jumper/Assets/Scripts/Education/Education.cs
@@ -22,9 +22,10 @@ public class Education : MonoBehaviour
 
     [SerializeField] private LanguageTranslator _language;
     [SerializeField] private PlayerMovement _movement;
-    [SerializeField] private CameraSpan _cameraSpan;
+    [SerializeField] private CameraSpan _cameraBrain;
     [SerializeField] private EducationPopup _popUp;
     [SerializeField] private EducationViewPoint[] _viewPoints;
+    [SerializeField] private GameObject _teacher;
 
     private Dictionary<Type, EducationViewPoint> _viewPointsMapped;
     private string[] _currentEducationPopupText;
@@ -32,6 +33,8 @@ public class Education : MonoBehaviour
 
     private void Start()
     {
+        _teacher.SetActive(false);
+        
         _viewPointsMapped = new Dictionary<Type, EducationViewPoint>();
         foreach (EducationViewPoint p in _viewPoints)
         {
@@ -39,14 +42,38 @@ public class Education : MonoBehaviour
         }
         
         _movement.Lock();
+        _teacher.SetActive(true);
         ShowEducation(Type.Start, () => {
             ShowEducation(Type.Ramp, () => {
+                _teacher.SetActive(false);
                 ShowEducation(Type.Trace, () => {
-                    _cameraSpan.ReturnBack();
+                    _cameraBrain.ReturnBack();
                     _movement.Unlock();
                 });
             });
         });
+
+        // _movement.Lock();
+        // _teacher.SetActive(true);
+        // ShowEducation(Type.SecondLevel, () => {
+        //     ShowEducation(Type.PetOpening, () => {
+        //         _teacher.SetActive(false);
+        //         ShowEducation(Type.Spinwheel, () => {
+        //             _cameraBrain.ReturnBack();
+        //             _movement.Unlock();
+        //         });
+        //     });
+        // });
+
+        //         _movement.Lock();
+        // ShowEducation(Type.Memes, () => {
+        //     ShowEducation(Type.SkinStore, () => {
+        //         ShowEducation(Type.Store, () => {
+        //             _cameraBrain.ReturnBack();
+        //             _movement.Unlock();
+        //         });
+        //     });
+        // });
     }   
 
     public void OnChangeLevel(int level)
@@ -57,7 +84,7 @@ public class Education : MonoBehaviour
             ShowEducation(Type.SecondLevel, () => {
                 ShowEducation(Type.PetOpening, () => {
                     ShowEducation(Type.Spinwheel, () => {
-                        _cameraSpan.ReturnBack();
+                        _cameraBrain.ReturnBack();
                         _movement.Unlock();
                     });
                 });
@@ -70,7 +97,7 @@ public class Education : MonoBehaviour
             ShowEducation(Type.Memes, () => {
                 ShowEducation(Type.SkinStore, () => {
                     ShowEducation(Type.Store, () => {
-                        _cameraSpan.ReturnBack();
+                        _cameraBrain.ReturnBack();
                         _movement.Unlock();
                     });
                 });
@@ -90,7 +117,7 @@ public class Education : MonoBehaviour
                 _currentEducationPopupText = new string[] 
                     {
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "Я ВСЕМОГУЩИЙ СИГМА-ПРЫГУН! ХРЮ" : "I AM THE ALMIGHTY SIGMA JUMPER! OINK",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ТЫ НИКОГДА НЕ СМОЖЕШЬ СТАТЬ ТАКИМ КАК Я! ХРЮ" : "YOU CAN NEVER BE LIKE ME! OINK",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ТЫ НИКОГДА НЕ СМОЖЕШЬ СТАТЬ ТАКИМ, КАК Я!" : "YOU CAN NEVER BE LIKE ME!",
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ХОТЯ МОЖЕШЬ ПОПРОБОВАТЬ..." : "ALTHOUGH YOU CAN TRY...",
 
                     };
@@ -98,16 +125,15 @@ public class Education : MonoBehaviour
             case Type.Ramp:
                 _currentEducationPopupText = new string[] 
                     {
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЭТО РАМПА, ХРЮ" : "IT'S A RAMP, OINK",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЕСЛИ ТЫ СИГМА, ТО РАЗБЕЖИСЬ..." : "IF YOU ARE SIGMA, THEN ACCELERATE...",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЕСЛИ ТЫ СИГМА, ТО РАЗБЕЖИСЬ ПО ЭТОЙ РАМПЕ И..." : "IF YOU ARE SIGMA, THEN ACCELERATE...",
                     };
                 break;
             case Type.Trace:
                 _currentEducationPopupText = new string[] 
                     {
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПОПРОБУЙ ДОПРЫГНУТЬ ДО КОНЦА" : "TRY TO JUMP TO THE END",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КОГДА ПРЫГАЕШЬ, УВЕЛИЧИВАЕТСЯ ТВОЯ СИЛА, ХРЮ" : "WHEN YOU JUMP, YOUR POWER INCREASES, OINK",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПРЫГАЙ СТОЛЬКО, ПОКА НЕ ДОПРЫГНЕШЬ ДО КОНЦА, ХРЮ" : "JUMP AS MUCH UNTIL YOU JUMP TO THE END, OINK",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КОГДА ПРЫГАЕШЬ, УВЕЛИЧИВАЕТСЯ ТВОЯ СИЛА" : "WHEN YOU JUMP, YOUR POWER INCREASES, OINK",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПРЫГАЙ, ПОКА НЕ ДОПРЫГНЕШЬ ДО КОНЦА" : "JUMP AS MUCH UNTIL YOU JUMP TO THE END",
                     };
 
                 break;
@@ -115,25 +141,25 @@ public class Education : MonoBehaviour
                 _currentEducationPopupText = new string[] 
                     {
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "О НЕЕЕТ... ТЫ РЕАЛЬНО ДОПРЫГНУЛ ДО КОНЦА???" : "OH NOOO...DID YOU REALLY JUMP TO THE END???",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ВОЗМОЖНО ТЫ ПРАВДА В БУДУЩЕМ СТАНЕШЬ СИГМОЙ, ХРЮ" : "MAYBE YOU WILL ACTUALLY BECOME SIGMA IN THE FUTURE, OINK",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "НО ЭТО ЕЩЕ НЕ ВСЕ, ТЕПЕРЬ ТРАССЫ БУДУТ СТАНОВИТСЯ ДЛИННЕЕ" : "BUT THAT'S NOT ALL, NOW THE TRAILS WILL BE LONGER",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ВОЗМОЖНО ТЫ ПРАВДА В БУДУЩЕМ СТАНЕШЬ СИГМОЙ" : "MAYBE YOU WILL ACTUALLY BECOME SIGMA IN THE FUTURE",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "НО ТРАССЫ БУДУТ ДЛИННЕЕ С КАЖДЫМ ЛЕВЕЛОМ..." : "BUT THAT'S NOT ALL, NOW THE TRAILS WILL BE LONGER",
                     };
                 break;
             case Type.PetOpening:
                 _currentEducationPopupText = new string[] 
                     {
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПРОХОДИ ДАЛЬШЕ, А ПОСЛЕ КАЖДОГО ЛЕВЕЛА, Я БУДУ ДАВАТЬ ТЕБЕ ЯЙЦО С ПИТОМЦЕМ" : "MOVE ON, AND AFTER EACH LEVEL, I WILL GIVE YOU AN EGG WITH A PET",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ИЗ ЯЙЦА МОГУТ ВЫПАСТЬ РАЗНЫЕ ПИТОМЦЫ, КОТОРЫЕ БУДУТ ПОМОГАТЬ ТЕБЕ" : "DIFFERENT PETS THAT WILL HELP YOU CAN FALL FROM THE EGG",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ОТКРОЙ И ПОЛУЧИ СВОЕГО ПЕРВОГО ПИТОМЦА, ХРЮ" : "OPEN AND GET YOUR FIRST PET, OINK",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "Я БУДУ ДАВАТЬ ТЕБЕ ЯЙЦО С РАЗНЫМИ ПИТОМЦАМИ КАЖДЫЙ ЛЕВЕЛ" : "MOVE ON, AND AFTER EACH LEVEL, I WILL GIVE YOU AN EGG WITH A PET",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ОНИ БУДУТ ПОМОГАТЬ ТЕБЕ" : "DIFFERENT PETS THAT WILL HELP YOU CAN FALL FROM THE EGG",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ОТКРОЙ И ПОЛУЧИ ЕГО ВПЕРВЫЕ!" : "OPEN AND GET YOUR FIRST PET",
                     };
                 break;
             case Type.Spinwheel:
                 _currentEducationPopupText = new string[] 
                     {
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЕСЛИ ТЫ ХОЧЕШЬ БЫСТРЕЕ ПОЛУЧИТЬ ДОПОЛНИТЕЛЬНОГО ПИТОМЦА ИЛИ МОНЕТОК..." : "IF YOU WANT TO GET AN ADDITIONAL PET OR COINS FASTER...",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КРУТИ КОЛЕСО И ПОЛУЧАЙ ПРИЗЫ, ХРЮ!" : "SPIN THE WHEEL AND EARN PRIZES, OINK!",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЕСЛИ ТЫ ХОЧЕШЬ БЫСТРЕЕ ПОЛУЧИТЬ ДОПОЛНИТЕЛЬНОГО ПИТОМЦА ИЛИ МОНЕТКИ..." : "IF YOU WANT TO GET AN ADDITIONAL PET OR COINS FASTER...",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КРУТИ КОЛЕСО И ПОЛУЧАЙ ПРИЗЫ!" : "SPIN THE WHEEL AND EARN PRIZES!",
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "НЕ БУДУ ЗАДЕРЖИВАТЬ ТЕБЯ, ТРЕНИРУЙСЯ" : "I WILL NOT DELAY YOU, TRAIN",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "СТАНОВИСЬ СИГМОЙ, ХРЮ!" : "BECOME SIGMA, OINK!",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "СТАНОВИСЬ СИГМОЙ!" : "BECOME SIGMA!",
 
                     };
                 break;
@@ -142,15 +168,15 @@ public class Education : MonoBehaviour
                 _currentEducationPopupText = new string[] 
                     {
                         _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ТЫ НЕВЕОРЯТЕН! НИ У КОГО ЕЩЕ НЕ ПОЛУЧАЛОСЬ ВЫЗВАТЬ ТАЙНУЮ КОМНАТУ" : "YOU ARE INCREDIBLE! NO ONE HAS BEEN ABLE TO CALL THE CHAMBER OF SECRETS",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПО СЛУХАМ, В НЕЙ МЕМЫ, ХРЮ" : "ACCORDING TO RUMOR, IT CONTAINS MEMES, OINK",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КАЖДЫЙ ЛЕВЕЛ КОМНАТА МЕНЯЕТСЯ, НО ЗАПАРКУРИТЬ ТУДА СТАНОВИТСЯ СЛОЖНЕЕ!" : "EACH LEVEL ROOM CHANGES, BUT PARKOURING THERE BECOMES MORE DIFFICULT",
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "В СВОБОДНОЕ ВРЕМЯ ЗАБЕРАЙСЯ ТУДА, ЗАРАБОТАЙ ДЕНЕГ" : "IN YOUR FREE TIME, GO THERE AND MAKE MONEY",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ПО СЛУХАМ, В НЕЙ МЕМЫ" : "ACCORDING TO RUMOR, IT CONTAINS MEMES",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "КАЖДЫЙ ЛЕВЕЛ, КОМНАТА МЕНЯЕТСЯ, НО ЗАПАРКУРИТЬ ТУДА СТАНОВИТСЯ СЛОЖНЕЕ!" : "EACH LEVEL ROOM CHANGES, BUT PARKOURING THERE BECOMES MORE DIFFICULT",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "В СВОБОДНОЕ ВРЕМЯ ЗАБИРАЙСЯ ТУДА, ЗАРАБАТЫВАЙ ДЕНЬГИ" : "IN YOUR FREE TIME, GO THERE AND MAKE MONEY",
                     };
                 break;  
             case Type.SkinStore:
                 _currentEducationPopupText = new string[] 
                     {
-                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЧТО У ТЕБЯ ЗА СКИН? КУПИ СЕБЕ НОРМАЛЬНЫЙ В МАГАЗИНЕ СКИНОВ, ЧТОБЫ ВЫГЛЯДЕТЬ КРУТО, ХРЮ" : "WHAT IS YOUR SKIN? BUY YOURSELF A NORMAL ONE IN THE SKIN STORE TO LOOK COOL, OINK",
+                        _language.CurrentLangunage == LanguageTranslator.Languages.Russian ? "ЧТО У ТЕБЯ ЗА СКИН? КУПИ СЕБЕ НОРМАЛЬНЫЙ В МАГАЗИНЕ СКИНОВ, ЧТОБЫ ВЫГЛЯДЕТЬ КРУТО" : "WHAT IS YOUR SKIN? BUY YOURSELF A NORMAL ONE IN THE SKIN STORE TO LOOK COOL",
                     };
                 break;
             case Type.Store:
@@ -161,7 +187,7 @@ public class Education : MonoBehaviour
                 break;  
         }
 
-        _cameraSpan.Span(point,
+        _cameraBrain.Span(point,
             () => {
                 _popUp.Show(_currentEducationPopupText, () => {
                     callback();
